a97091e22689d0ac56c5e226fe0fc06b5d45d325
Patrice DARGENTON <patrice.dargenton@free.fr>
2024-08-04 08:44:42 +0200

    Plus de copyright

--- Src/AssemblyInfo.vb
+++ Src/AssemblyInfo.vb
@@ -7,9 +7,9 @@ Imports System.Runtime.InteropServices
     "VBFileFind : Pour remplacer la recherche de fichiers de Windows, " &
     "d'après File Searcher in C# By Manfred Bittersam, 24/04/2009, " &
     "https://www.codeproject.com/Articles/35044/File-Searcher-in-C")>
-<Assembly: AssemblyCompany("ORS Production")> 
+<Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("VBFileFind")>
-<Assembly: AssemblyCopyright("Copyright © ORS Production 2024")>
+<Assembly: AssemblyCopyright("Copyright © 2024")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: AssemblyVersion("1.0.9.*")>
